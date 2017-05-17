7966808ce1b0b5e0a1f09491ff9189e7e8401c59
jkaminallen <kaminallenj@gmail.com>
2017-05-17 14:22:18 -0400

    setting up future admin use

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @products = Product.all
   end
@@ -6,4 +8,29 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: "Successfully added product"
+    else
+      render :new
+    end
+  end
+
+  protected
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+  def authorize_user
+    if !user_signed_in? || !current_user.admin?
+      raise ActionController::RoutingError.new("Not Found")
+    end
+  end 
 end
