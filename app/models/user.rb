509f31c26d48df2cdd900394625494e24ac94204
jkaminallen <kaminallenj@gmail.com>
2017-06-01 13:06:07 -0400

    make sure email is proper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +8,6 @@ class User < ApplicationRecord
   def admin?
     role == "admin"
   end
+
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
